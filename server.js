df4d929deda979d449163cf162fcba8a1285c4d2
ranibangar <ranibangar111@gmail.com>
2024-05-18 10:29:43 +0530

    changes done

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import {Server} from "socket.io";
 import cors from "cors";
 import http from "http";
-
+import {connect} from "./config.js"
+import { chatModel } from "./chat.schema.js";
 const app=express()
 
 //1.create server using http
@@ -19,11 +20,42 @@ const io = new Server(server,{
 //3.use socket events
 io.on('connection',(socket)=>{
     console.log("connection is established");
+
+    socket.on("join",(data)=>{
+        socket.username=data;
+
+        //Display messages n 
+        //sort latest messages ans show nly 50 msg
+        chatModel.find().sort({timestamp:1}).limit(50)
+        .then(messages=>{
+            socket.emit('load_messages',messages);
+        }).catch(err=>{
+            console.log(err);
+        })
+    })
+    socket.on('new_message',(message)=>{   //new_message is from client
+
+        // to add username with message
+        let userMessage={
+            username:socket.username,
+            message:message
+        }
+   //to save data to DB
+   const newChat=new chatModel({
+    username:socket.username,
+    message:message,
+    timestamp:new Date()
+   })
+   newChat.save();
+        //broadcast this message to all the clients
+        socket.broadcast.emit('broadcast_message',userMessage);
+    })
     socket.on('disconnect',()=>{
         console.log("Connection is disconnected")
     });
 })
 
 server.listen(3000,()=>{
-    console.log("App is listening on port 3000")
+    console.log("App is listening on port 3000");
+    connect();
 })
